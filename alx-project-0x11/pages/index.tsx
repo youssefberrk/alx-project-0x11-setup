c40ab64e03e06f2c9a391f47d3ad21a2cb059da3
youssef <yberrakouan.lol@gmail.com>
2025-01-16 11:31:02 +0100

    Track images generated

--- alx-project-0x11/pages/index.tsx
+++ alx-project-0x11/pages/index.tsx
@@ -1,9 +1,11 @@
 import ImageCard from "@/components/common/ImageCard";
+import { ImageProps } from "@/interfaces";
 import React, { useState } from "react";
 
 const Home: React.FC = () => {
   const [prompt, setPrompt] = useState<string>("");
   const [imageUrl, setImageUrl] = useState<string>("");
+  const [generatedImages, setGeneratedImages] = useState<ImageProps[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const handleGenerateImage = async () => {
@@ -25,6 +27,11 @@ const Home: React.FC = () => {
 
     const data = await resp.json();
     setIsLoading(false);
+    setImageUrl(data?.message);
+    setGeneratedImages((prev) => [
+      ...prev,
+      { imageUrl: data?.message, prompt },
+    ]);
   };
 
   return (
@@ -59,6 +66,25 @@ const Home: React.FC = () => {
           />
         )}
       </div>
+      {generatedImages.length ? (
+        <div className="">
+          <h3 className="text-xl text-center mb-4">Generated Images</h3>
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 border max-w-full md:max-w-[1100px] p-2 overflow-y-scroll h-96">
+            {generatedImages?.map(({ imageUrl, prompt }: ImageProps, index) => (
+              <ImageCard
+                action={() => setImageUrl(imageUrl)}
+                imageUrl={imageUrl}
+                prompt={prompt}
+                key={index}
+                width="w-full"
+                height="h-40"
+              />
+            ))}
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
